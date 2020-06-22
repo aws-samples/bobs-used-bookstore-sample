4f96634d38a46c948504219677491e35b9289c97
ancelmop <ancelmop@DESKTOP-LJ2QFL3>
2020-06-22 10:25:41 -0400

    Created Initial Ui pages for the Views related to Orders

---
+++ BOBS-Backend/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BOBS_Backend.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessOrders()
+        {
+            return View();
+        }
+    }
+}
