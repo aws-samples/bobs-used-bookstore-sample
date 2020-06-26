62275527ed3f1b2820a8e5a1218e479f41f1e4e2
brian <bleffew@amazon.com>
2020-06-26 11:24:34 -0400

    initial model for Customer created

---
+++ BobBookstore/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BobBookstore.Models
+{
+    public class Customer
+    {
+        [Required]
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public string DOB { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+    }
+}
