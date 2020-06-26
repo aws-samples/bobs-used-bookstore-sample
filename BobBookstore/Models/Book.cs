d6ed7dedc8071324b3b0105729798a08b053df3d
brian <bleffew@amazon.com>
2020-06-26 11:30:04 -0400

    initial Book Model create

---
+++ BobBookstore/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BobBookstore.Models
+{
+    public class Book
+    {
+        [Required]
+        public int ID { get; set; }
+        public int PublisherID { get; set; }
+        public string Name { get; set; }
+        public int TypeID { get; set; }
+        public int PriceID { get; set; }
+        public int ISBN { get; set; }
+    }
+}
