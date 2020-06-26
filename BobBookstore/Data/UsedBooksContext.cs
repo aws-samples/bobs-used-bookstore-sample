4dca00f37cfe2278914fbace846cbc3c285ba05c
brian <bleffew@amazon.com>
2020-06-26 12:52:21 -0400

    initialize dbcontext file

---
+++ BobBookstore/Data/UsedBooksContext.cs
@@ -0,0 +1,22 @@
+﻿using BobBookstore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BobBookstore.Data
+{
+    public class UsedBooksContext : DbContext
+    {
+        public UsedBooksContext(DbContextOptions<UsedBooksContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
