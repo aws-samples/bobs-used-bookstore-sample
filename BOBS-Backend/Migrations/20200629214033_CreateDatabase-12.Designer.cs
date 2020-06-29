f615912f233bae9135dc6aaa42e1d9faa9066d1c
ancelmop <ancelmop@DESKTOP-LJ2QFL3>
2020-06-29 17:46:06 -0400

    Updated Address Model

--- BOBS-Backend/Migrations/20200629214033_CreateDatabase-12.Designer.cs
+++ BOBS-Backend/Migrations/20200629214033_CreateDatabase-12.Designer.cs
@@ -4,14 +4,16 @@ using BOBS_Backend.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BOBS_Backend.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200629214033_CreateDatabase-12")]
+    partial class CreateDatabase12
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BOBS-Backend/Migrations/20200629214033_CreateDatabase-12.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BOBS_Backend.Migrations
+{
+    public partial class CreateDatabase12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrimary",
+                table: "Address",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrimary",
+                table: "Address");
+        }
+    }
+}
