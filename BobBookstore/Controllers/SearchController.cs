0a034faa4eb76d836f40c81badff53a6ba059265
brian <bleffew@amazon.com>
2020-06-30 10:42:58 -0400

    update search page

--- BobBookstore/Controllers/SearchController.cs
+++ BobBookstore/Controllers/SearchController.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BobBookstore.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BobBookstore.Controllers
 {
     public class SearchController : Controller
     {
-        public IActionResult Index()
+        private readonly UsedBooksContext _context;
+
+        public SearchController(UsedBooksContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FirstOrDefaultAsync(m => m.Book_Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
         }
     }
 }
