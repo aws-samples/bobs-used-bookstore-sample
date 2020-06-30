4e1cfc276d9dcf4ed60fb126e7cec78cda07d205
ancelmop <ancelmop@amazon.com>
2020-06-30 16:43:57 -0400

    Finishing adding basic comments

--- BOBS-Backend/ViewModel/ProcessOrders/ProcessOrderViewModel.cs
+++ BOBS-Backend/ViewModel/ProcessOrders/ProcessOrderViewModel.cs
@@ -8,6 +8,10 @@ namespace BOBS_Backend.ViewModel.ProcessOrders
 {
     public class ProcessOrderViewModel
     {
+        /*
+         * ProccssOrder ViewModel
+         * Containes Order Id and New Status Id to Update Order Status
+         */
         public long OrderId { get; set; }
 
         public long Status { get; set; }
