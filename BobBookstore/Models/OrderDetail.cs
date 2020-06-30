0c97ea13ba9d914102fb2830d627450194af0ba2
brian <bleffew@amazon.com>
2020-06-30 10:17:55 -0400

    connection strings

--- BobBookstore/Models/OrderDetail.cs
+++ BobBookstore/Models/OrderDetail.cs
@@ -12,8 +12,8 @@ namespace BobBookstore.Models
         public long OrderStatus_Id { get; set; }
         public Order Order { get; set; }
         public Book Book { get; set; }
-        public Price Price_Id { get; set; }
-        public double Price { get; set; }
+        public Price Price { get; set; }
+        public double price { get; set; }
         public int Quantity { get; set; }
     }
 }
