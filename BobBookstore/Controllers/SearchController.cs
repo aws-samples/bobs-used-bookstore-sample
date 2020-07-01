510d023f860d53beb3fc8f65b0988254b1073a2b
Leffew <bleffew@amazon.com>
2020-07-01 12:06:28 -0400

    search page initial commit

--- BobBookstore/Controllers/SearchController.cs
+++ BobBookstore/Controllers/SearchController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BobBookstore.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BobBookstore.Controllers
 {
     public class SearchController : Controller
     {
-        public IActionResult Index()
+        private readonly UsedBooksContext _context;
+
+        public SearchController(UsedBooksContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            return View(await _context.Book.ToListAsync());
         }
     }
 }
