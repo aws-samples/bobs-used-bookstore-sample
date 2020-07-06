4f0ab16f8bb044689668ead2bdb7971b1c5ead66
brian <bleffew@amazon.com>
2020-07-06 15:31:26 -0400

    order view page initial

---
+++ BobBookstore/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BobBookstore.Models.ViewModels
+{
+    public class OrderViewModel
+    {
+        public double Subtotal { get; set; }
+        public double Tax { get; set; }
+        public string DeliveryDate { get; set; }
+        public string Status { get; set; }
+    }
+}
