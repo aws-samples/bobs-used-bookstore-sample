4dcd99085c2eaddfcfdbefb34e24ef872971d01f
Leffew <bleffew@amazon.com>
2020-07-07 10:22:02 -0400

    orders

--- BobBookstore/Controllers/OrdersController.cs
+++ BobBookstore/Controllers/OrdersController.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BobBookstore.Data;
+using BobBookstore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BobBookstore.Controllers
 {
     public class OrdersController : Controller
     {
-        public IActionResult Index()
+        private readonly UsedBooksContext _context;
+
+        public OrdersController(UsedBooksContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> IndexAsync(long id)
+        {
+            var orders = from o in _context.Order where o.Customer.Customer_Id == id
+                         select new OrderViewModel() 
+                         {
+                             Status = o.OrderStatus.Status,
+                             Tax = o.Tax,
+                             Subtotal = o.Subtotal,
+                             DeliveryDate = o.DeliveryDate
+                         };
+
+
+            return View(await orders.ToListAsync());
         }
     }
 }
