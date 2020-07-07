c9d97661a41e5bc8ca52db902911d203cfca349d
Brian Leffew <bleffew99@gmail.com>
2020-07-07 10:53:50 -0400

    re-add search

--- BobBookstore/Controllers/SearchController.cs
+++ BobBookstore/Controllers/SearchController.cs
@@ -2,15 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BobBookstore.Data;
+using BobBookstore.Models.Book;
+using BobBookstore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BobBookstore.Controllers
 {
     public class SearchController : Controller
     {
-        public IActionResult Index()
+        private readonly UsedBooksContext _context;
+
+        public SearchController(UsedBooksContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> IndexAsync(string searchString)
+        {
+
+            var books = from b in _context.Book
+                        select new BookViewModel()
+                        {
+                            BookId = b.Book_Id,
+                            BookName = b.Name,
+                            ISBN = b.ISBN,
+                            GenreName = b.Genre.Name,
+                            TypeName = b.Type.TypeName,
+                            Url = b.Back_Url
+                        };
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(s => s.BookName.Contains(searchString) || s.GenreName.Contains(searchString)
+                                    || s.TypeName.Contains(searchString));
+            }
+
+            return View(await books.ToListAsync());
+        }
+
+        public IActionResult Detail(long id)
+        {
+            var book = from m in _context.Book
+                       where m.Book_Id == id
+                       select new BookViewModel()
+                       {
+                           BookName = m.Name,
+                           ISBN = m.ISBN,
+                           GenreName = m.Genre.Name,
+                           TypeName = m.Type.TypeName,
+                           Url = m.Back_Url
+                       };
+
+            return View(book.First());
         }
     }
 }
---
+++ BobBookstore/Models/ViewModels/BookViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BobBookstore.Models.ViewModels
+{
+    public class BookViewModel
+    {
+        public long BookId { get; set; }
+        public string BookName { get; set; }
+        public long ISBN { get; set; }
+        public string GenreName { get; set; }
+        public string TypeName { get; set; }
+        public string Url { get; set; }
+    }
+}
