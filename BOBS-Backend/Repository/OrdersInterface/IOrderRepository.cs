8c65a13035455f4278f174180bd05c9d6b1ccb5a
ancelmop <ancelmop@amazon.com>
2020-07-09 13:17:00 -0400

    Update filter logic to be more dynamic and improved some Order UI

--- BOBS-Backend/Repository/OrdersInterface/IOrderRepository.cs
+++ BOBS-Backend/Repository/OrdersInterface/IOrderRepository.cs
@@ -12,15 +12,7 @@ namespace BOBS_Backend.Repository.OrdersInterface
 
         /*
          * Order Detail Repository Interface
-         */
-
-        ManageOrderViewModel RetrieveViewModel(string filterValue, string searchString, int pageNum, int totalPages, int[] pages, List<Order> order);
-
-        IQueryable<Order> GetBaseOrderQuery();
-
-        int GetTotalPages(IQueryable<Order> orderQuery);
-
-        Task<ManageOrderViewModel> RetrieveFilterViewModel(IQueryable<Order> filterQuery, int totalPages, int pageNum, string filterValue, string searchString);
+         */ 
 
         Task<Order> FindOrderById(long id);
 
