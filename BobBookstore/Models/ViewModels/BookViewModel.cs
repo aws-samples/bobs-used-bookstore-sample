db8f033405a232406b36040eddb30c45a25a8e0b
brian <bleffew@amazon.com>
2020-07-16 14:18:51 -0400

    sort function

--- BobBookstore/Models/ViewModels/BookViewModel.cs
+++ BobBookstore/Models/ViewModels/BookViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,6 +16,26 @@ namespace BobBookstore.Models.ViewModels
         public List<Book.Price> Prices { get; set; }
         public string Url { get; set; }
 
-        
+
+        public static void sortBy(List<BookViewModel> lst, string prop)
+        {
+            switch (prop)
+            {
+                case "Name":
+                    lst.Sort((elt1, elt2) => elt1.BookName.CompareTo(elt2.BookName));
+                    break;
+                case "Genre":
+                    lst.Sort((elt1, elt2) => elt1.GenreName.CompareTo(elt2.GenreName));
+                    break;
+                case "Type":
+                    lst.Sort((elt1, elt2) => elt1.TypeName.CompareTo(elt2.TypeName));
+                    break;
+                case "Price":
+                    lst.Sort((elt1, elt2) => elt1.Prices.Min().ItemPrice.CompareTo(elt2.Prices.Min().ItemPrice));
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
