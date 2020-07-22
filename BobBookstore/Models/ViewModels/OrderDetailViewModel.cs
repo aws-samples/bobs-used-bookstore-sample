6204c9dec867a5bef17fe03cee08ebba0cd47693
yiming pan <2802008635@qq.com>
2020-07-22 16:41:00 -0400

    almost finish checkout

---
+++ BobBookstore/Models/ViewModels/OrderDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BobBookstore.Models.ViewModels
+{
+    public class OrderDetailViewModel
+    {
+        public string Bookname { get; set; }
+        public int quantity { get; set; }
+        public string Url { get; set; }
+        public double  price { get; set; }
+    }
+}
