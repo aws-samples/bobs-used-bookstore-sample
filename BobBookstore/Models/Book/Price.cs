710176d9d6a9148050a85a09fa5c5e18ba480001
Leffew <bleffew@a483e7c65d1d.ant.amazon.com>
2020-07-27 11:14:16 -0400

    updated models to reflect quip doc

--- BobBookstore/Models/Book/Price.cs
+++ BobBookstore/Models/Book/Price.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,18 +9,29 @@ namespace BobBookstore.Models.Book
 {
     public class Price
     {
-        [Key]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Price_Id { get; set; }
 
         public Book Book { get; set; }
 
         public Condition Condition { get; set; }
 
+        [RegularExpression(@"^\d+\.\d{0,2}$")]
+        [Range(0, 9999999999999999.99)]
         public double ItemPrice { get; set; }
 
         public int Quantity { get; set; }
         public string UpdatedBy { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime UpdatedOn { get; set; }
+
+        public bool Active { get; set; }
+
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
 
     }
 }
