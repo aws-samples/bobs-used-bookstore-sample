718b4749e636ca1c5eb57b3895b69b2c10b52032
jbbhide <jbbhide@DESKTOP-BH4CUNF>
2020-07-29 18:44:12 -0700

    error controller change

--- BOBS-Backend/Controllers/ErrorController.cs
+++ BOBS-Backend/Controllers/ErrorController.cs
@@ -2,55 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Routing;
 
 namespace BOBS_Backend.Controllers
 {
     public class ErrorController : Controller
     {
-        private readonly ILogger<ErrorController> logger;
-
-        public ErrorController(ILogger<ErrorController> logger)
+        [Route("/Error/Index/{code:int}")]
+        public IActionResult Index(int code)
         {
-            this.logger = logger;
+
+
+            var exception = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            ViewData["Path"] = exception?.Path;
+            var error = Problem();
+
+            ViewData["StatusCode"] = code;
+            return View();
         }
 
-        [AllowAnonymous]
-        [Route("Error")]
-        public IActionResult Error()
+        [Route("/error")]
+        public IActionResult Support()
         {
-            // Retrieve the exception Details
-            var exceptionHandlerPathFeature =
-                HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-            // LogError() method logs the exception under Error category in the log
-            logger.LogError($"The path {exceptionHandlerPathFeature.Path} " +
-                $"threw an exception {exceptionHandlerPathFeature.Error}");
+            var exception = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            ViewData["Path"] = exception?.Path;
+            var error = Problem();
 
-            return View("Error");
-        }
-
-        [Route("Error/{statusCode}")]
-        public IActionResult HttpStatusCodeHandler(int statusCode)
-        {
-            var statusCodeResult =
-                HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
-
-            switch (statusCode)
-            {
-                case 404:
-                    ViewBag.ErrorMessage = "Sorry, the resource could not be found";
-                    // LogWarning() method logs the message under
-                    // Warning category in the log
-                    logger.LogWarning($"404 error occured. Path = " +
-                        $"{statusCodeResult.OriginalPath} and QueryString = " +
-                        $"{statusCodeResult.OriginalQueryString}");
-                    break;
-            }
-
-            return View("NotFound");
+            ViewData["StatusCode"] = error.StatusCode;
+            return View("~/Views/Error/Index.cshtml");
         }
     }
 }
