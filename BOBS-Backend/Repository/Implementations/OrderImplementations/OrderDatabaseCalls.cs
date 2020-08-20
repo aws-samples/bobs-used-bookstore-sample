3c8f133ae649b3d31911d3e70707084888e916af
ancelmop <ancelmop@amazon.com>
2020-08-20 14:26:38 -0400

    Hopefully fix

---
+++ BOBS-Backend/Repository/Implementations/OrderImplementations/OrderDatabaseCalls.cs
@@ -0,0 +1,70 @@
+﻿using BOBS_Backend.Database;
+using BOBS_Backend.Models.Order;
+using BOBS_Backend.Repository.OrdersInterface;
+using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BOBS_Backend.Repository.Implementations.OrderImplementations
+{
+    public class OrderDatabaseCalls : IOrderDatabaseCalls
+    {
+
+        private DatabaseContext _context;
+
+        public OrderDatabaseCalls(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable GetBaseQuery(string objPath)
+        {
+            var query = _context.Query(objPath);
+
+            return query;
+        }
+
+        public IQueryable<Order> ReturnBaseOrderQuery(IQueryable query, string[] includes)
+        {
+            var result = (IQueryable<Order>)query;
+            result = result.Include(includes);
+
+            return result;
+        }
+
+        public IQueryable<Order> ReturnFilterOrderQuery(IQueryable<Order> query,Expression<Func<Order,bool>> lambda)
+        {
+            return query.Where(lambda);
+        }
+
+        public IQueryable<OrderStatus> ReturnBaseOrderStatusQuery(IQueryable query, string[] includes)
+        {
+            var result = (IQueryable<OrderStatus>)query;
+            result = result.Include(includes);
+
+            return result;
+        }
+
+        public IQueryable<OrderStatus> ReturnFilterOrderStatusQuery(IQueryable<OrderStatus> query, Expression<Func<OrderStatus, bool>> lambda)
+        {
+            return query.Where(lambda);
+        }
+
+        public IQueryable<OrderDetail> ReturnBaseOrderDetailQuery(IQueryable query, string[] includes)
+        {
+            var result = (IQueryable<OrderDetail>)query;
+            result = result.Include(includes);
+
+            return result;
+        }
+
+        public IQueryable<OrderDetail> ReturnFilterOrderDetailQuery(IQueryable<OrderDetail> query, Expression<Func<OrderDetail, bool>> lambda)
+        {
+            return query.Where(lambda);
+        }
+
+    }
+}
--- BOBS-Backend/Repository/Implementations/OrderImplementations/OrderRepository.cs
+++ BOBS-Backend/Repository/Implementations/OrderImplementations/OrderRepository.cs
@@ -29,6 +29,7 @@ namespace BOBS_Backend.Repository.Implementations.OrderImplementations
          * Order Repository contains all functions associated with Order Model
          */
 
+        private DatabaseContext _context;
         private readonly int _ordersPerPage = 20;
         private readonly string[] OrderIncludes = { "Customer", "Address", "OrderStatus" };
         private ISearchRepository _searchRepo;
@@ -36,8 +37,9 @@ namespace BOBS_Backend.Repository.Implementations.OrderImplementations
         private IExpressionFunction _expFunc;
 
         // Set up connection to Database 
-        public OrderRepository(ISearchRepository searchRepo, IOrderDatabaseCalls orderDbCalls, IExpressionFunction expFunc)
+        public OrderRepository(DatabaseContext context, ISearchRepository searchRepo, IOrderDatabaseCalls orderDbCalls, IExpressionFunction expFunc)
         {
+            _context = context;
             _searchRepo = searchRepo;
             _orderDbCalls = orderDbCalls;
             _expFunc = expFunc;
@@ -85,7 +87,7 @@ namespace BOBS_Backend.Repository.Implementations.OrderImplementations
 
             var orderBase = _orderDbCalls.GetBaseQuery("BOBS_Backend.Models.Order.Order");
 
-            var query = _orderDbCalls.ReturnBaseQuery<Order>(orderBase, OrderIncludes);
+            var query = _orderDbCalls.ReturnBaseOrderQuery(orderBase, OrderIncludes);
 
             var totalPages = _searchRepo.GetTotalPages(query.Count(),_ordersPerPage);
 
@@ -130,7 +132,7 @@ namespace BOBS_Backend.Repository.Implementations.OrderImplementations
         {
 
             ManageOrderViewModel viewModel = new ManageOrderViewModel();
-            var parameterExpression = _expFunc.ReturnParameterExpression(typeof(Order), "order");
+            var parameterExpression = Expression.Parameter(Type.GetType("BOBS_Backend.Models.Order.Order"), "order");
 
 
             var expression = _searchRepo.ReturnExpression(parameterExpression, filterValue, searchString);
@@ -147,9 +149,9 @@ namespace BOBS_Backend.Repository.Implementations.OrderImplementations
             }
             var orderBase = _orderDbCalls.GetBaseQuery("BOBS_Backend.Models.Order.Order");
 
-            var query = _orderDbCalls.ReturnBaseQuery<Order>(orderBase, OrderIncludes);
+            var query = _orderDbCalls.ReturnBaseOrderQuery(orderBase, OrderIncludes);
 
-            var filterQuery = _orderDbCalls.ReturnFilterQuery<Order>(query, lambda);
+            var filterQuery = _orderDbCalls.ReturnFilterOrderQuery(query, lambda);
 
             int totalPages = _searchRepo.GetTotalPages(filterQuery.Count(), _ordersPerPage);
 
@@ -170,14 +172,17 @@ namespace BOBS_Backend.Repository.Implementations.OrderImplementations
             //var negateTest = "false true";
 
             string tableName = "Order";
+            var parameterExpression = Expression.Parameter(typeof(Order), "order");
 
-            Expression<Func<Order, bool>> lambda = _expFunc.ReturnLambdaExpression<Order>(tableName, filterValue, searchString, inBetween, operand, negate);
+            var expression = _expFunc.ReturnExpression(filterValue, tableName,parameterExpression, searchString, inBetween, operand, negate);
+
+            Expression<Func<Order, bool>> lambda = Expression.Lambda<Func<Order, bool>>(expression, parameterExpression);
 
             var orderBase = _orderDbCalls.GetBaseQuery("BOBS_Backend.Models.Order.Order");
 
-            var query = _orderDbCalls.ReturnBaseQuery<Order>(orderBase, OrderIncludes);
+            var query = _orderDbCalls.ReturnBaseOrderQuery(orderBase, OrderIncludes);
 
-            var filterQuery = _orderDbCalls.ReturnFilterQuery(query, lambda);
+            var filterQuery = _orderDbCalls.ReturnFilterOrderQuery(query, lambda);
 
             return filterQuery;
         }
