ede9a1eceb0f6dcbae2b3fd07d5ff3c690c4e49f
Steve Roberts <strobe@amazon.com>
2022-03-30 15:56:28 -0700

    Update infrastructure project to CDKv2, and .NET 6

---
+++ infra/BookstoreInfra/src/BookstoreInfra/Program.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.CDK;
+
+namespace BookstoreInfra
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new CoreResourceStack(app, "BobsBookstoreCoreInfra", new StackProps
+            {
+                Env = MakeEnv(null, "us-west-2")
+            });
+
+            app.Synth();
+        }
+
+        private static Amazon.CDK.Environment MakeEnv(string account = null, string region = null)
+        {
+            return new Amazon.CDK.Environment
+            {
+                Account = account ??
+                    System.Environment.GetEnvironmentVariable("CDK_DEPLOY_ACCOUNT") ??
+                    System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
+                Region = region ??
+                    System.Environment.GetEnvironmentVariable("CDK_DEPLOY_REGION") ??
+                    System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION")
+            };
+        }
+    }
+}
