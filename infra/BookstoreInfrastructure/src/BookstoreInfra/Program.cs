143df833496e577ff28149d569972505fd034082
Brad Webber <bradweb@amazon.com>
2022-09-14 17:46:16 -0500

    Removed hard-coded dependency on us-west-2 in CDK.

--- infra/BookstoreInfrastructure/src/BookstoreInfra/Program.cs
+++ infra/BookstoreInfrastructure/src/BookstoreInfra/Program.cs
@@ -7,24 +7,8 @@ internal sealed class Program
     public static void Main(string[] args)
     {
         var app = new App();
-        new CoreResourceStack(app, "BobsBookstoreCoreInfra", new StackProps
-        {
-            Env = MakeEnv(null, "us-west-2")
-        });
+        new CoreResourceStack(app, "BobsBookstoreCoreInfra");
 
         app.Synth();
-    }
-
-    private static Environment MakeEnv(string account = null, string region = null)
-    {
-        return new Environment
-        {
-            Account = account ??
-                      System.Environment.GetEnvironmentVariable("CDK_DEPLOY_ACCOUNT") ??
-                      System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
-            Region = region ??
-                     System.Environment.GetEnvironmentVariable("CDK_DEPLOY_REGION") ??
-                     System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION")
-        };
-    }
+    }   
 }
