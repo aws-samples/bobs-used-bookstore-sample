8d9abee82daa79a33d744b49f8be801c1a6b032e
Brad Webber <bradweb@amazon.com>
2022-09-14 17:47:05 -0500

    Updated AdminSite app start up to align with minimal hosting model.

--- app/AdminApplication/AdminSite/DbInitializer.cs
+++ app/AdminApplication/AdminSite/DbInitializer.cs
@@ -1,36 +1,11 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Transfer;
-using DataAccess.Data;
-using DataModels.Books;
-using Microsoft.Extensions.Configuration;
 using System.IO;
-using System.Linq;
 
 namespace AdminSite
 {
     public static class DbInitializer
     {
-        public static void Initialize(ApplicationDbContext context, string bucketName, string cdnDomain, string path)
-        {
-            context.Database.EnsureCreated();
-            System.Diagnostics.Trace.WriteLine("message");
-
-           /* if (context.Book.Any())
-            {
-                return;
-            }*/
-           /* var books = new Book[]
-            {
-                new Book{Author = "Test Lastname", Publisher = new Publisher{Name = "Stupore Bookhouse" }, ISBN = "6775546678", Type = new Type{TypeName = "AudioBook" }, Name = "Stupore Cries", Genre = new Genre{ Name = "Horror"}, FrontUrl = uploadToS3(bucketName, cdnDomain, path, "stupore.jpg") }
-            };
-
-            foreach (Book s in books)
-            {
-                context.Book.Add(s);
-            }
-            context.SaveChanges();*/
-        }
-
         public static string uploadToS3(string bucketName, string cdnDomain, string path, string bookCover)
         {
             IAmazonS3 S3Client = new AmazonS3Client();
--- app/AdminApplication/AdminSite/Program.cs
+++ app/AdminApplication/AdminSite/Program.cs
@@ -1,41 +1,84 @@
-using Microsoft.AspNetCore.Hosting;
+using Amazon.Polly;
+using Amazon.Rekognition;
+using Amazon.S3;
+using Amazon.Translate;
+using DataAccess.Data;
+using DataAccess.Repository.Implementation;
+using DataAccess.Repository.Implementation.InventoryImplementation;
+using DataAccess.Repository.Implementation.NotificationsImplementations;
+using DataAccess.Repository.Implementation.OrderImplementations;
+using DataAccess.Repository.Implementation.SearchImplementation;
+using DataAccess.Repository.Implementation.WelcomePageImplementation;
+using DataAccess.Repository.Interface;
+using DataAccess.Repository.Interface.Implementations;
+using DataAccess.Repository.Interface.InventoryInterface;
+using DataAccess.Repository.Interface.NotificationsInterface;
+using DataAccess.Repository.Interface.OrdersInterface;
+using DataAccess.Repository.Interface.SearchImplementations;
+using DataAccess.Repository.Interface.WelcomePageInterface;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NLog.Web;
 
-namespace AdminSite
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            /*var logger = NLog.Web.NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
-            
-            logger.Debug("init main");*/
-            CreateHostBuilder(args).Build().Run();
-            
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        private static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, builder) =>
-                {
-                    builder.AddSystemsManager("/BobsUsedBookAdminStore/");
-                    builder.AddSystemsManager("/bookstoredb/");
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                }).ConfigureLogging(logging =>
-                {
-                    logging.AddAWSProvider();
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddAWSService<IAmazonS3>();
+builder.Services.AddAWSService<IAmazonPolly>();
+builder.Services.AddAWSService<IAmazonRekognition>();
+builder.Services.AddAWSService<IAmazonTranslate>();
+builder.Services.AddCognitoIdentity();
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("BobsBookstoreContextConnection")));
+builder.Services.AddTransient<ISearchDatabaseCalls, SearchDatabaseCalls>();
+builder.Services.AddTransient<IExpressionFunction, ExpressionFunction>();
+builder.Services.AddTransient<IOrderDatabaseCalls, OrderDatabaseCalls>();
+builder.Services.AddTransient<IInventory, Inventory>();
+builder.Services.AddTransient<IRekognitionNPollyRepository, RekognitionNPollyRepository>();
+builder.Services.AddTransient<ISearchRepository, SearchRepository>();
+builder.Services.AddTransient<IOrderRepository, OrderRepository>();
+builder.Services.AddTransient<IOrderDetailRepository, OrderDetailRepository>();
+builder.Services.AddTransient<IOrderStatusRepository, OrderStatusRepository>();
+builder.Services.AddTransient<INotifications, Notifications>();
+builder.Services.AddTransient<ICustomAdminPage, CustomAdmin>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
-                    // When you need logging below set the minimum level. Otherwise the logging framework will default to Informational for external providers.
-                    logging.SetMinimumLevel(LogLevel.Debug);
-                }).UseNLog();
-        } 
-        
-    }
+// Configure AWS Logging
+builder.Logging.AddAWSProvider();
+
+// Add AWS Systems Manager 
+builder.Configuration.AddSystemsManager("/BobsUsedBookAdminStore/", optional:true);
+builder.Configuration.AddSystemsManager("/bookstoredb/", optional:true);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{   
+    app.UseExceptionHandler("/Error");
+    
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+// Configure Middleware
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
+// Create the database
+using (var scope = app.Services.CreateAsyncScope())
+{
+    await scope.ServiceProvider.GetService<ApplicationDbContext>()!.Database.EnsureCreatedAsync();
+}
+
+app.Run();
