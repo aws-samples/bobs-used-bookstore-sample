1bc4332b6e5034083710bb18b392f4ec8ea09065
Brad Webber <bradweb@amazon.com>
2023-01-04 17:20:10 -0600

    Updated program.cs.

--- infrastructure/IntegratedTest/src/IntegratedTestInfrastructure/Program.cs
+++ infrastructure/IntegratedTest/src/IntegratedTestInfrastructure/Program.cs
@@ -4,15 +4,24 @@ namespace IntegratedTestInfrastructure;
 
 internal sealed class Program
 {
+    // The target account and region can be controlled by hardcoding them in this class
+    // (recommended for production workloads) or using --profile on the CDK CLI.
+    //
+    // Hardcode the account and region by passing them as parameters to the MakeEnv() 
+    // method, e.g. MakeEnv("8373873873", "us-west-2")
+    //
+    // Specify the account and region from the CDK CLI by specifying a profile to use, 
+    // e.g. cdk deploy --profile "USWest2Profile"
+    //
+    // If the account and region are not hardcoded and a profile is not specified on the CLI
+    // the CDK will use the account and region of the default profile.
+    // 
+    // For more information refer to https://docs.aws.amazon.com/cdk/v2/guide/environments.html
     public static void Main(string[] args)
     {
         var app = new App();
-        _ = new IntegratedTestStack(app, "BobsUsedBooksIntegratedTest", new StackProps
-        {
-            // if no explicit region is set on synth/deployment, or set in environment variables,
-            // default to US West (Oregon)
-            Env = MakeEnv(null, "us-west-2")
-        });
+
+        _ = new IntegratedTestStack(app, "BobsUsedBooksIntegratedTest", new StackProps { Env = MakeEnv() });
 
         app.Synth();
     }
@@ -21,12 +30,8 @@ internal sealed class Program
     {
         return new Environment
         {
-            Account = account ??
-                      System.Environment.GetEnvironmentVariable("CDK_DEPLOY_ACCOUNT") ??
-                      System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
-            Region = region ??
-                     System.Environment.GetEnvironmentVariable("CDK_DEPLOY_REGION") ??
-                     System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION")
+            Account = account ?? System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
+            Region = region ?? System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION")
         };
     }
 }
