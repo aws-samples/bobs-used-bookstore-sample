622b6b22098fe4c13efbb54ed510e684e715742c
Brad Webber <bradweb@amazon.com>
2023-04-25 14:01:24 -0500

    Update view model to accommodate repopulating reference data

--- app/Bookstore.Web/Areas/Admin/Models/Inventory/InventoryCreateUpdateViewModel.cs
+++ app/Bookstore.Web/Areas/Admin/Models/Inventory/InventoryCreateUpdateViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using Bookstore.Domain.Books;
 using Bookstore.Domain.ReferenceData;
+using Bookstore.Web.Helpers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -13,21 +16,7 @@ namespace Bookstore.Web.Areas.Admin.Models.Inventory
 
         public InventoryCreateUpdateViewModel(IEnumerable<ReferenceDataItem> referenceDataItems)
         {
-            BookConditions = referenceDataItems
-                .Where(x => x.DataType == ReferenceDataType.Condition)
-                .Select(x => new SelectListItem(x.Text, x.Id.ToString()));
-            
-            BookTypes = referenceDataItems
-                .Where(x => x.DataType == ReferenceDataType.BookType)
-                .Select(x => new SelectListItem(x.Text, x.Id.ToString()));
-            
-            Genres = referenceDataItems
-                .Where(x => x.DataType == ReferenceDataType.Genre)
-                .Select(x => new SelectListItem(x.Text, x.Id.ToString()));
-            
-            Publishers = referenceDataItems
-                .Where(x => x.DataType == ReferenceDataType.Publisher)
-                .Select(x => new SelectListItem(x.Text, x.Id.ToString()));
+            AddReferenceData(referenceDataItems);
         }
 
         public InventoryCreateUpdateViewModel(IEnumerable<ReferenceDataItem> referenceDataItems, Book book) : this(referenceDataItems)
@@ -49,33 +38,72 @@ namespace Bookstore.Web.Areas.Admin.Models.Inventory
 
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Author { get; set; }
 
         public int Year { get; set; }
 
+        [Required]
         public string ISBN { get; set; }
 
         public IEnumerable<SelectListItem> Publishers { get; set; } = new List<SelectListItem>();
+        
+        [Required]
+        [DisplayName("Publisher")]
         public int SelectedPublisherId { get; set; }
 
         public IEnumerable<SelectListItem> BookTypes { get; set; } = new List<SelectListItem>();
+
+        [Required]
+        [DisplayName("Book Type")]
         public int SelectedBookTypeId { get; set; }
 
         public IEnumerable<SelectListItem> Genres { get; set; } = new List<SelectListItem>();
+       
+        [Required]
+        [DisplayName("Genre")]
         public int SelectedGenreId { get; set; }
 
         public IEnumerable<SelectListItem> BookConditions { get; set; } = new List<SelectListItem>();
+        
+        [Required]
+        [DisplayName("Condition")]
         public int SelectedConditionId { get; set; }
 
+        [Required]
         public decimal Price { get; set; }
 
+        [Required]
         public int Quantity { get; set; } = 1;
 
+        [MaxFileSize(2*1024*1024)]
+        [DisplayName("Cover image")]
         public IFormFile CoverImage { get; set; }
+        
         public string CoverImageUrl { get; set; }
 
         public string Summary { get; set; }
+
+        public void AddReferenceData(IEnumerable<ReferenceDataItem> referenceDataItems)
+        {
+            BookConditions = referenceDataItems
+                .Where(x => x.DataType == ReferenceDataType.Condition)
+                .Select(x => new SelectListItem(x.Text, x.Id.ToString()));
+
+            BookTypes = referenceDataItems
+                .Where(x => x.DataType == ReferenceDataType.BookType)
+                .Select(x => new SelectListItem(x.Text, x.Id.ToString()));
+
+            Genres = referenceDataItems
+                .Where(x => x.DataType == ReferenceDataType.Genre)
+                .Select(x => new SelectListItem(x.Text, x.Id.ToString()));
+
+            Publishers = referenceDataItems
+                .Where(x => x.DataType == ReferenceDataType.Publisher)
+                .Select(x => new SelectListItem(x.Text, x.Id.ToString()));
+        }
     }
 }
